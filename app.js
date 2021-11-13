28daefdf0944d5fe92f58794c4bdbc985a6bc413
Woodstock <victorhe97@gmail.com>
2021-11-13 14:48:35 -0500

    MongoDB is now connected.

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+
+app.get('/', (req, res) =>
+  res.send('Testing the best MERN Twitter by Emmay and Victor!')
+);
+
+const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err));
 
-const users = require('./routes/api/users');
-const tweets = require('./routes/api/tweets');
-
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Testing the best MERN Twitter by Emmay and Victor!'));
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
 
