61c9e92c4bbf99a272494855e52da85bb33ec345
Woodstock <victorhe97@gmail.com>
2021-11-13 14:57:45 -0500

    Added the models directory and created a User model.

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 
-app.get('/', (req, res) =>
-  res.send('Testing the best MERN Twitter by Emmay and Victor!')
-);
-
-const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err));
+
+app.get('/', (req, res) =>
+  res.send('Testing the best MERN Twitter by Emmay and Victor!')
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
