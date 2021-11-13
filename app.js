ececb1204883df94f7151530c119073eabad4b9a
Woodstock <victorhe97@gmail.com>
2021-11-13 13:05:42 -0500

    Added config file with the db key, added additional lines of code within app.js to connect to mongoose. Added files to git ignore folder.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const db = require('./config/keys').mongoURI;
 
-app.get('/', (req, res) => res.send('Hello World'));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('MERN Twitter by Emmay and Victor!'));
 
 // change this port later to run on Heroku
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
-
-
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb://<DBUser>:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority',
+  //Make sure this is your own unique string
+};
