8ac563d8961c38daca6439509803f5134f7e7d95
Emmay Alam <88454447+Emmay-Alam@users.noreply.github.com>
2021-11-13 13:46:57 -0500

    Delete config directory

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-  // TODO: Change the User and Password
-    'mongodb://emmay:YdSDd2ULcJi4GTv@cluster.mongodb.net/test?retryWrites=true&w=majority',
-  // TODO: Make sure this is your own unique string
-};
