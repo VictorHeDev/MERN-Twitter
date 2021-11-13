fd797097d7bf81e0fb741940ba0c1e0f88764660
Woodstock <victorhe97@gmail.com>
2021-11-13 12:51:12 -0500

    Added express, simple get request, and specified local port to run server.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// change this port later to run on Heroku
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+
