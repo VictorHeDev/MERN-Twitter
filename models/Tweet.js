0f23b1ca19c1df4edbd12c2c9deea55a0f9c2361
Emmay <emmayalam1@gmail.com>
2021-11-13 17:04:49 -0500

    Added Tweet model.

---
+++ models/Tweet.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TweetSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users' // * ref is foreign key
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Tweet = mongoose.model('Tweet', TweetSchema);
+module.exports = Tweet;
+;
