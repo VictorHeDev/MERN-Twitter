8e7013bb9d23533a0cd4b9d1e18dc6a25db5bf30
Woodstock <victorhe97@gmail.com>
2021-11-14 14:53:52 -0500

    Completed project.

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,10 +3,12 @@
 import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer.js';
 import errors from './errors_reducer';
+import tweets from './tweets_reducer';
 
 const RootReducer = combineReducers({
   session: sessionReducer,
   errors,
+  tweets,
 });
 
 export default RootReducer;
